2dbcc27a077cdb1f4f7e01d62e5931c13a50523a
Nakula <nakulamarvellio@gmail.com>
2021-05-04 05:49:52 +0700

    ADD: Setup folder

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCEti0RaBoZOWd28bES4EebXkhFtldzA4M",
+    authDomain: "netflixnmclone.firebaseapp.com",
+    projectId: "netflixnmclone",
+    storageBucket: "netflixnmclone.appspot.com",
+    messagingSenderId: "547920320555",
+    appId: "1:547920320555:web:9bf7c31b34838d11f3726e"
+  };
