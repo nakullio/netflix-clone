6ffde1cb16b7f65a719c62a06bb259d3de3c7b7c
Nakula <nakulamarvellio@gmail.com>
2021-05-06 05:32:09 +0700

    ADD: Build Row section & styling

--- src/Banner.js
+++ src/Banner.js
@@ -33,17 +33,19 @@ function Banner() {
     return (
         <header className="banner" style={{
             backgroundSize: 'cover',
-            backgroundImage: `url('https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png')`,
+            backgroundImage: `url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
             backgroundPosition: 'center center',
         }} >
         
         <div className="banner__contents">
-            <h1 className="banner__title">Movie Name</h1>
+            <h1 className="banner__title">
+                {movie?.title || movie?.name || movie?.original_name}
+            </h1>
             <div className="banner__buttons">
                 <button className="banner__button" >Play</button>
                 <button className="banner__button" >My List</button>
             </div>
-            <h1 className="banner__description">{truncate(`TLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`, 150)} </h1>
+            <h1 className="banner__description">{truncate(movie?.overview, 150)} </h1>
         </div>
             
             <div className="banner--fadeBottom" />
