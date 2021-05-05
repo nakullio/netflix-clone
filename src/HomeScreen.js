0df9b5cd1f3820902cb3f06a366403c8398bb5bc
Nakula <nakulamarvellio@gmail.com>
2021-05-06 05:34:21 +0700

    CHNG: Cleanup build

--- src/HomeScreen.js
+++ src/HomeScreen.js
@@ -9,15 +9,12 @@ function HomeScreen() {
     return (
         <div className="homeScreen">
             <Nav />
-
             <Banner />
-
             <Row 
                 title='NETFLIX ORIGINALS'
                 fetchUrl={requests.fetchNetflixOriginals}
                 isLargeRow
             />
-
             <Row title='Trending Now' fetchUrl={requests.fetchTrending}/>
             <Row title='Top Rated' fetchUrl={requests.fetchTopRated}/>
             <Row title='Action Movies' fetchUrl={requests.fetchActionMovies}/>
@@ -25,10 +22,6 @@ function HomeScreen() {
             <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies}/>
             <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies}/>
             <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries}/>
-
-
-
-            
         </div>
     )
 }
