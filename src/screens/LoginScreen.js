0942f592e709427f4fe906842afe30f836b0f9ab
Nakula <nakulamarvellio@gmail.com>
2021-05-10 06:59:37 +0700

    ADD: Building the Login Screen

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -4,7 +4,18 @@ import './LoginScreen.css'
 function LoginScreen() {
     return (
         <div className='loginScreen'>
-            
+            <div className="loginScreen__background">
+                <img className='loginScreen__logo' src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png" alt=""/>
+                <button className='loginScreen__button' >Sign In</button>
+                <div className="loginScreen__gradient"></div>
+            </div>
+
+            <div className="loginScreen__body">
+                <>
+                    <h1>Unlimited films, TV programmes and more.</h1>
+                    <h2>Watch anywhere. Cancel at any time.</h2>
+                </>
+            </div>
         </div>
     )
 }
