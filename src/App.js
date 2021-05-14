bccc3503c58c13dca0926ecf4e0f6ea040374c20
Nakula <nakulamarvellio@gmail.com>
2021-05-14 08:56:31 +0700

    ADD: Implementing Redux

--- src/App.js
+++ src/App.js
@@ -8,20 +8,28 @@ import {
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
 import { auth } from './firebase';
+import { useDispatch, useSelector } from 'react-redux';
+import { login, logout, selectUser } from './features/userSlice';
 
 
 
 function App() {
-  const user = null;
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
 
   // keep persistent signIn, using useEffect
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(userAuth => {
       if (userAuth) {
         // logged in
-        console.log(userAuth)
+        console.log(userAuth);
+        dispatch(login({
+          uid: userAuth.uid,
+          email: userAuth.email,
+        }))
       } else {
         // logged out
+        dispatch(logout);
       }
     });
 
