2acfe24d62172af587a8576feedb5a41fac000d7
Nakula <nakulamarvellio@gmail.com>
2021-05-14 07:24:23 +0700

    ADD: Build & Styling Signup screen

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -4,7 +4,17 @@ import './SignupScreen.css'
 function SignupScreen() {
     return (
         <div className="signupScreen">
-            
+            <form>
+                <h1>Sign In</h1>
+                <input  placeholder='Email' type="email" />
+                <input placeholder='Password' type="password" />
+                <button type='submit' >Sign In</button>
+
+                <h4>
+                    <span className='signupScreen__gray' >New to Neflix? </span>
+                    <span className='signupScreen__link' >Sign Up now.</span>
+                    </h4>
+            </form>
         </div>
     )
 }
