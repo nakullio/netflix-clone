d84e59091816a162c12c46645344301449cc3f46
Nakula <nakulamarvellio@gmail.com>
2021-05-14 08:02:34 +0700

    ADD: Setting up Authentication

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import {
@@ -7,11 +7,27 @@ import {
   Route,
 } from "react-router-dom";
 import LoginScreen from './screens/LoginScreen';
+import { auth } from './firebase';
 
 
 
 function App() {
   const user = null;
+
+  // keep persistent signIn, using useEffect
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // logged in
+        console.log(userAuth)
+      } else {
+        // logged out
+      }
+    });
+
+
+    return unsubscribe;
+  }, [])
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase';
+
+
 const firebaseConfig = {
     apiKey: "AIzaSyCEti0RaBoZOWd28bES4EebXkhFtldzA4M",
     authDomain: "netflixnmclone.firebaseapp.com",
@@ -6,3 +9,10 @@ const firebaseConfig = {
     messagingSenderId: "547920320555",
     appId: "1:547920320555:web:9bf7c31b34838d11f3726e"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth }
+  export default db;
