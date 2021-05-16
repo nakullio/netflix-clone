c3024cda62a9872f4e25188a8cc95e0a19c9f7fa
Nakula <nakulamarvellio@gmail.com>
2021-05-16 07:35:08 +0700

    ADD: Styling planscreen & setup stripe integration

--- src/screens/PlansScreen.js
+++ src/screens/PlansScreen.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
 import db from '../firebase';
 import './PlansScreen.css'
+import { loadStripe } from '@stripe/stripe-js';
 
 function PlansScreen() {
     const [products, setProducts] = useState([]);
+    const user = useSelector(selectUser);
 
     // using useEffect to fetch the plan, which has been created with firebase-stripe integration
     useEffect(() => {
@@ -26,9 +30,49 @@ function PlansScreen() {
 
     console.log(products)
 
+    // use async function
+    const loadCheckout = async (priceId) => {
+        const docRef = await db.collection('customers').doc(user.uid).collection('checkout_sessions')
+        .add({
+            price: priceId,
+            succes_url: window.location.origin,
+            cancel_url: window.location.origin,
+        });
+
+        docRef.onSnapshot(async(snap) => {
+            const {error, sessionId} = snap.data();
+
+            if (error) {
+                // Show an error to your customer and
+                // inspect your Cloud Function logs in the Firebase console.
+                alert(`An error occured: ${error.message}`);
+            }
+
+            if (sessionId) {
+                // We have a session, let's redirect to Checkout
+                // init Stripe
+
+                const stripe = await loadStripe('publish_keyxxxxxxx');
+                stripe.redirectToCheckout({ sessionId })
+            }
+        })
+    }
+
     return (
         <div className='planScreen'>
-            
+            {Object.entries(products).map(([productId, productData]) => {
+                // add some logic to check if the user's subscription is active
+                return (
+                    <div className="plansScreen__plan">
+                        <div className="plansScreen__info">
+                            <h5>{productData.name}</h5>
+                            <h6>{productData.description}</h6>
+                        </div>
+
+                        <button onClick={() => loadCheckout(productData.prices.priceId)} >Subscribe</button>
+                    </div>
+                )
+            })}
         </div>
     )
 }
